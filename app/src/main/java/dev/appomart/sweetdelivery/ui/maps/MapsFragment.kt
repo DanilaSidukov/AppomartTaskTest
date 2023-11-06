84671cc9671a005dd23041707ecbda45a28e5fdd
DanilaSidukov <sidukov01@mail.ru>
2023-11-06 23:49:43 +0300

    all functionality implemented

--- app/src/main/java/dev/appomart/sweetdelivery/ui/maps/MapsFragment.kt
+++ app/src/main/java/dev/appomart/sweetdelivery/ui/maps/MapsFragment.kt
@@ -67,12 +67,5 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        // Add a marker in Sydney and move the camera
-//        val sydney = LatLng(-34.0, 151.0)
-//        mMap.addMarker(
-//            MarkerOptions()
-//            .position(sydney)
-//            .title("Marker in Sydney"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(mMap.cameraPosition))
     }
 }
